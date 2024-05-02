5cfb8189f7b0e62753b84038db258ef6a04450d2
pocoyo7798 <daniel.pereira.costa@tecnico.ulisboa.pt>
2024-05-02 13:29:30 +0100

    changes to setup and delete comments

--- src/zs4procext/extractor.py
+++ src/zs4procext/extractor.py
@@ -289,7 +289,7 @@ class SamplesExtractorFromText(BaseModel):
         else:
             self._llm_model = ModelLLM(model_name=self.llm_model_name)
         self._llm_model.load_model_parameters(llm_param_path)
-        #self._llm_model.vllm_load_model()
+        self._llm_model.vllm_load_model()
         atributes = ["name", "preparation", "yield"]
         self._schema_parser = SchemaParser(atributes_list=atributes)
         self._schema_parser.model_post_init(None)
