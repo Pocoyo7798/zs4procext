b1a4ee9d386e28d6f1082856834e40d61f0b8864
pocoyo7798 <daniel.pereira.costa@tecnico.ulisboa.pt>
2024-06-28 17:12:48 +0100

    add worker_usae_ray to llm

--- src/zs4procext/llm.py
+++ src/zs4procext/llm.py
@@ -47,6 +47,7 @@ class ModelLLM(BaseModel):
                     "enforce_eager": self.model_parameters["enforce-eager"],
                     "quantization": self.model_parameters["quantization"],
                     "max_model_len": self.model_parameters["max_model_len"],
+                    "worker_use_ray": self.model_parameters["worker-use-ray"]
                     #"distributed_executor_backend": self.model_parameters["distributed-executor-backend"]
                 },
             )
